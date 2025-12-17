8bff7e45c0e7e088165e30b192641928432577aa
haithem satoutah <satoutahhaithem@gmail.com>
2025-12-17 15:08:07 +0100

    add case sensitive

--- match_control_flow/src/main.rs
+++ match_control_flow/src/main.rs
@@ -2,16 +2,18 @@ use std::io;
 
 fn main() {
     println!("Please enter a greeting:");
-
     let mut name = String::new();
-    io::stdin()
-        .read_line(&mut name)
-        .expect("Failed to read input");
+    io::stdin().read_line(&mut name).expect("Failed to read input");
+
+    let greeting = name.trim().to_lowercase();
 
     // use of match expression to pattern match against variable "name"
-    match name.trim() {
-        "Good Bye" => println!("Sorry to see you go."),
-        "Hello" => println!("Hi, nice to meet you!"),
+    match greeting.trim() {
+        "good bye" => println!("Sorry to see you go."),
+        "hello" => println!("Hi, nice to meet you!"),
+        "good morning" => println!("Good morning"),
+        "good evening" => println!("Good evening"),
         _ => println!("I can't find a greeting, good bye."),
     }
 }
+
